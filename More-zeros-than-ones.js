498c7920fb2a3140ce6f2a849f36e4ca94052c3b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-01 21:42:08 -0800

    Update More-zeros-than-ones.js

--- More-zeros-than-ones.js
+++ More-zeros-than-ones.js
@@ -22,3 +22,10 @@ Leading zeros in binary should not be counted.
 
 
 // Solution
+
+function moreZeros(s){
+  let arrCodePoints = s.split('').map(c => c.charCodeAt(0)).map(n => n.toString(2));
+  let chars = arrCodePoints.filter(s => s.split('0').length > s.split('1').length).map(s => parseInt(s, 2));
+  let uniq = new Set(chars);
+  return String.fromCharCode(...Array.from(uniq)).split('')
+}
